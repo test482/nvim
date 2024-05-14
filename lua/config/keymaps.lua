42f2c7e7fd456a92e83a777d5ce9885dbb80dacd
test482 <eliotjoking@gmail.com>
2024-05-15 06:58:18 +0800

    code_runner: lazy load it

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local keymap = vim.keymap
+-- local keymap = vim.keymap
 
--- code runner
-keymap.set("n", "<leader>crf", ":CRFiletype<CR>", { silent = true, desc = "code runner files json" })
-keymap.set("n", "<leader>crp", ":CRProjects<CR>", { silent = true, desc = "code runner projects json" })
-keymap.set("n", "<leader>r", ":RunCode<CR>", { silent = false, desc = "Run Code" })
-keymap.set("n", "<leader>rf", ":RunFile<CR>", { silent = false, desc = "[F]ile" })
-keymap.set("n", "<leader>rp", ":RunProject<CR>", { silent = false, desc = "[P]roject" })
-keymap.set("n", "<leader>rt", ":RunFile tab<CR>", { silent = false, desc = "[T]ab mode" })
-keymap.set("n", "<leader>rw", ":RunClose<CR>", { silent = false, desc = "Close runner" })
--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -4,6 +4,18 @@ return {
     config = true,
     opts = {
       filetype_path = vim.fn.expand('~/.config/nvim/code_runner.json'),
-    }
+    },
+    keys = {
+      { "<leader>r",   ":RunCode<CR>",     silent = false,          desc = "Run Code" },
+
+      { "<leader>rf",  ":RunFile<CR>",     silent = false,          desc = "[F]ile" },
+      { "<leader>rp",  ":RunProject<CR>",  silent = false,          desc = "[P]roject" },
+      { "<leader>rt",  ":RunFile tab<CR>", silent = false,          desc = "[T]ab mode" },
+      { "<leader>rw",  ":RunClose<CR>",    silent = false,          desc = "Close runner" },
+
+      { "<leader>rx",  "",                 desc = "Edit Json files" },
+      { "<leader>rxf", ":CRFiletype<CR>",  silent = true,           desc = "code runner filetype json" },
+      { "<leader>rxp", ":CRProjects<CR>",  silent = true,           desc = "code runner projects json" },
+    },
   }
 }
